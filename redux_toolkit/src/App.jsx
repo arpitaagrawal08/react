6078473d7f754979d6c9e8b0efd36128f562806a
arpitaagrawal08 <168635973+arpitaagrawal08@users.noreply.github.com>
2025-05-28 00:41:09 +0530

    redux toolkit

---
+++ redux_toolkit/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn about redux tool-kit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
